dc212aa6195f1186a1bd05773be95da6e705c0e5
joshismayank <joshismayank@gmail.com>
2020-12-17 01:56:09 -0700

    added mrdt experiment skeleton

--- simple_list_merge.ml
+++ simple_list_merge.ml
@@ -192,3 +192,82 @@ let test_merge () = Lwt_main.run begin
     (* check if local val is equal to what is desired *)
     (* assert ([1L;2L;1L;4L;5L] = s) *)
 end;;
+
+let n_rounds = 10;;
+
+let n_ops_per_commit_start = 10;;
+
+let n_ops_per_commit_end = 100;;
+
+let n_ops_per_commit_gap = 10;;
+
+let rec delete_at_pos list pos new_list =
+    match list with 
+        | [] -> new_list
+        | x::xs -> if pos = 0 then delete_at_pos xs (pos-1) new_list
+            else delete_at_pos xs (pos-1) (x::new_list)
+
+let do_one_op curr_list =
+    let curr_rand_int = Random.int 9 in
+    if curr_rand_int < 8 then 
+        let curr_rand_int64 = Random.int64 Int64.max_int in 
+        (curr_rand_int64::curr_list)
+    else let curr_rand_pos = Random.int (List.length curr_list) in 
+        delete_at_pos curr_list curr_rand_pos [];;
+
+let rec do_total_ops total curr list = 
+    if curr < total then let new_list = do_one_op list in
+        do_total_ops total (curr+1) new_list
+    else list;;
+
+let get_list_from_master = Lwt_main.run begin 
+    Git_store.Repo.v git_config >>= Git_store.master >>= fun master -> 
+    Git_store.get master ["path"] >|= fun s -> s end;;
+
+let get_list_from_local = Lwt_main.run begin 
+    Git_store.Repo.v git_config >>= fun repo -> 
+    Git_store.of_branch repo "local" >>= fun local -> 
+    Git_store.get local ["path"] >|= fun s -> s end;;
+
+let commit_in_master list = Lwt_main.run begin
+    Git_store.Repo.v git_config >>= Git_store.master >>= fun master -> 
+    Git_store.set_exn master ["path"] list ~info:(info "commiting in master") end;;
+
+(* commit val in local *)
+let commit_in_local list = Lwt_main.run begin
+    Git_store.Repo.v git_config >>= fun repo -> 
+    Git_store.of_branch repo "local" >>= fun local -> 
+    Git_store.set_exn local ["path"] list ~info:(info "commiting in local") end;;
+
+let run_experiment_for_one_commit n_ops_per_commit = 
+    (* get list from master *)
+    let master_list = get_list_from_master in
+    (* get list from local *)
+    let local_list = get_list_from_local in
+    (* do n_ops_per_commit no of ops on master list *)
+    let new_master_list = do_total_ops n_ops_per_commit 0 master_list in
+    (* do n_ops_per_commit no of ops on local list *)
+    let new_local_list = do_total_ops n_ops_per_commit 0 local_list in
+    (* commit master list in master *)
+    let _ = commit_in_master new_master_list in
+    (* commit local list in local *)
+    commit_in_local new_local_list;;
+    (* merge into local ?? *)
+    (* merge into master ?? *)
+
+let rec iter terminate gap curr param = 
+    if curr < terminate then
+        let _ = run_experiment_for_one_commit param in 
+        iter terminate gap (curr+gap) param;;
+
+let rec perform_varying_ops_per_commit terminate gap curr =
+    if curr < terminate then
+        let _ = iter n_rounds 1 1 curr in
+        perform_varying_ops_per_commit terminate gap (curr+gap);;
+
+let main () =
+    begin
+    perform_varying_ops_per_commit n_ops_per_commit_end n_ops_per_commit_gap n_ops_per_commit_start
+    end;;
+
+main ();;
